60697920fffd776f7e43ac6d1caaca96beeff3e3
jebafaria123 <70385712+jebafaria123@users.noreply.github.com>
2024-10-27 23:02:01 +0600

    Network_Access.py

--- Network_Access.py
+++ Network_Access.py
@@ -2,9 +2,9 @@ from netmiko import ConnectHandler
 
 device = {
     'device_type': 'cisco_ios',
-    'host': '192.168.137.6',
-    'username': 'jeba',
-    'password': 'jeba1234',
+    'host': 'Host_IP',
+    'username': 'Your_user_Name',
+    'password': 'Your_Password',
    
     
 }
